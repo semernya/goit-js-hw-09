da81d4cf81ffeb1b66ae1d20ce206cb54d2f1b2d
iknowueterent <semernya38@gmail.com>
2023-12-31 18:43:21 +0200

    complete 1, 3 points of task 2

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const key = 'feedback-form-state';
+const obj = {
+  email: '',
+  message: '',
+};
+
+// const testObj = { email: 'email', message: 'message' };
+
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onSubmitForm);
+
+// console.log(obj.email);
+// localStorage.setItem(
+//   'test-obj',
+//   JSON.stringify(testObj)
+// );
+
+    // if (localStorage.length != 0) {
+    //     const savedObj = JSON.parse(localStorage.getItem('test-obj'));
+    //     // console.log(savedObj);
+    //     input.value = savedObj.email;
+    //     textarea.value = savedObj.message;
+    // }
+
+function onInputForm(event) {
+  event.preventDefault();
+  (obj.email = input.value), (obj.message = textarea.value);
+  localStorage.setItem(key, JSON.stringify(obj));
+}
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  console.log(obj);
+  input.value = '';
+  textarea.value = '';
+  localStorage.removeItem(key);
+}
